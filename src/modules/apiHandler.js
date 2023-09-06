ae53c301bdbc9fd1bf914075244cbd50142405f4
Mike Plant <26470248+mikeplant@users.noreply.github.com>
2023-09-06 14:53:42 +0100

    Add display info funcs, hourly forecast scroller, remove unnecessary fetchCurrent func

--- src/modules/apiHandler.js
+++ src/modules/apiHandler.js
@@ -3,40 +3,24 @@ const apiHandler = (() => {
   const weatherApiUrlForecast = "https://api.weatherapi.com/v1/forecast.json";
   const apiKey = "5b67dd39869a4d9b8be85347230109";
 
-  let currentWeatherObj = null;
   let forecastWeatherObj = null;
 
-  const fetchCurrent = async (location) => {
-    try {
-      const url = `${weatherApiUrlCurrent}?key=${apiKey}&q=${location}&aqi=no`;
-      const response = await fetch(url, { mode: "cors" });
-      const weatherData = await response.json();
-      currentWeatherObj = weatherData.current;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const fetchForecast = async (location) => {
+  const fetchWeather = async (location = "London") => {
     try {
       const url = `${weatherApiUrlForecast}?key=${apiKey}&q=${location}&days=3&aqi=no&alerts=no`;
       const response = await fetch(url, { mode: "cors" });
       const weatherData = await response.json();
-      forecastWeatherObj = weatherData.forecast;
+      forecastWeatherObj = weatherData;
     } catch (error) {
       console.log("Error: " + error);
     }
   };
 
-  const fetchWeather = async (givenLocation = "London") => {
-    let location = givenLocation;
-    await fetchCurrent(location);
-    await fetchForecast(location);
-    console.log({ currentWeatherObj, forecastWeatherObj });
-  };
+  const getForecast = () => forecastWeatherObj;
 
   return {
     fetchWeather,
+    getForecast,
   };
 })();
 
--- src/modules/displayHandler.js
+++ src/modules/displayHandler.js
@@ -3,17 +3,75 @@ import apiHandler from "./apiHandler.js";
 const displayHandler = (() => {
   const locationForm = document.querySelector("#location-form");
   const locationInput = document.querySelector("#location-input");
+  const nextHourly = document.querySelector(".hourly-right");
+  const lastHourly = document.querySelector(".hourly-left");
+  const dayReel = document.querySelector("#day-reel");
+  let hourForecastPos = 0;
 
   const bindEvents = () => {
     locationForm.addEventListener("submit", (e) => {
       e.preventDefault();
-      handleInputSubmit();
+      updateWeather(locationInput.value);
     });
+    nextHourly.addEventListener("click", () => scrollHourForecastForwards());
+    lastHourly.addEventListener("click", () => scrollHourForecastBackwards());
   };
 
-  const handleInputSubmit = () => {
-    apiHandler.fetchWeather(locationInput.value);
+  const scrollHourForecastForwards = () => {
+    if (hourForecastPos == 2720) return;
+    hourForecastPos += 1360;
+    dayReel.style.right = hourForecastPos + "px";
+  };
+
+  const scrollHourForecastBackwards = () => {
+    if (hourForecastPos == 0) return;
+    hourForecastPos -= 1360;
+    dayReel.style.right = hourForecastPos + "px";
+  };
+
+  const displayLocation = (locationObj) => {
+    const cityEl = document.querySelector(".city");
+    const regionEl = document.querySelector(".region");
+    const countryEl = document.querySelector(".country");
+    cityEl.textContent = locationObj.name;
+    regionEl.textContent = locationObj.region;
+    countryEl.textContent = locationObj.country;
+  };
+
+  const displayCurrentCondition = (condition) => {
+    const currentCondition = document.querySelector(".current-condition");
+    currentCondition.textContent = condition;
+  };
+
+  const displayCurrentTemp = (temp) => {
+    const currentTemp = document.querySelector(".current-temp");
+    currentTemp.textContent = temp;
+  };
+
+  const displayCurrentFeelsLike = (temp) => {
+    const currentFeelsLike = document.querySelector(".current-feels-like");
+    currentFeelsLike.textContent = temp;
+  };
+
+  const displayCurrentPrecip = (precip) => {
+    const currentPrecip = document.querySelector(".current-precip");
+    currentPrecip.textContent = precip;
+  };
+
+  const displayCurrent = () => {
+    const currentWeatherObj = apiHandler.getForecast();
+    displayLocation(currentWeatherObj.location);
+    displayCurrentCondition(currentWeatherObj.current.condition.text);
+    displayCurrentTemp(currentWeatherObj.current.temp_c);
+    displayCurrentFeelsLike(currentWeatherObj.current.feelslike_c);
+    displayCurrentPrecip(currentWeatherObj.current.precip_mm);
+  };
+
+  const updateWeather = async (location) => {
+    await apiHandler.fetchWeather(location);
+    displayCurrent();
   };
 
   bindEvents();
+  updateWeather();
 })();
